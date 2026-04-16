04cd799d67f84194506018418c61b5f1993ee134
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-04-16 11:12:55 -0700

    added both tests and fixed getRelativeFrequency Method

--- src/CharacterFrequencyCounter.java
+++ src/CharacterFrequencyCounter.java
@@ -60,7 +60,7 @@ public class CharacterFrequencyCounter {
      */
     public double getRelativePercentage(char c) {
         c = Character.toLowerCase(c);
-        int charCount = frequencyMap.get(c);
+        int charCount = frequencyMap.getOrDefault(c, 0);
 
         return ((double) charCount / totalCharacterCount) * 100;
     }
--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,25 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
+
+    @Test
+    void TestCharacterShowsUpMultipleTimesGivesCorrectFrequency() {
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+        //Act
+        int actual = counter.getFrequency('l');
+        //Assert
+        assertEquals(2, actual);
+    }   
     
+    @Test
+    void TestGetRelativeFrequencyReturns0ForMissingCharacter() {
+        //arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hellow");
+
+        //act
+        double actual = counter.getRelativePercentage('x');
+        //assert
+        assertEquals(0.0, actual, 0.0001);
+    }
 }
